a53acd3e84b67254697cdab9e2cc7bc25b88dcbc
Iamandii, Alin <alin.iamandii@centric.eu>
2020-07-06 17:01:29 +0300

    add very hot option

--- TripLooking/Controllers/WeatherForecastController.cs
+++ TripLooking/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TripLooking.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Very Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
