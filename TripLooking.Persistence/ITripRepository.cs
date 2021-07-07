70f6a70109f9ce2a8f9b1199ef253dda8ac76028
Bogdan Tirniceriu <Bogdan.Tirniceriu@centric.eu>
2021-07-07 17:10:48 +0300

    Repository

---
+++ TripLooking.Persistence/ITripRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using TripLooking.Entities.Trips;
+
+namespace TripLooking.Persistence
+{
+    public interface ITripRepository
+    {
+        Task<Trip> GetTripById(Guid id);
+    }
+}
---
+++ TripLooking.Persistence/TripsRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using TripLooking.Entities.Trips;
+
+namespace TripLooking.Persistence
+{
+    public class TripsRepository : ITripRepository
+    {
+        private TripsContext context;
+
+        public TripsRepository(TripsContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Trip> GetTripById(Guid id)
+        {
+            return await this.context.Trips.FindAsync(id);
+        }
+    }
+}
